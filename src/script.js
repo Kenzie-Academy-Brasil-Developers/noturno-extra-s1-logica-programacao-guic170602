7b10f7a63c2db135d5b09d1f8615a6659ee27b54
GuilhermeColhado1 <guilhermecolhado1@gmail.com>
2021-10-29 20:05:30 -0300

    Alter function cashMachine

--- src/script.js
+++ src/script.js
@@ -26,7 +26,10 @@ function calculateAge(anoNasc,anoAtual){
     let mes=ano*12
     let dia=ano*365
     let semana=ano*52.143
-    return `A sua idade atualmente é: ${ano} \nA sua idade em meses é: ${mes} \nA sua idade em dias é: ${dia} \nA sua idade em semanas é aproximadamente: ${semana}`
+    return `A sua idade atualmente é: ${ano} anos
+A sua idade em meses é: ${mes}
+A sua idade em dias é: ${dia}
+A sua idade em semanas é aproximadamente: ${semana}`
 }
 console.log(calculateAge(2002,2021))
 console.log("-------------------------------------")
@@ -49,40 +52,42 @@ function cashMachine(r){
     let nota5
     let nota2
     let nota1
+    let output = ''
     if(r>=100){
         nota100=Math.trunc(r/100)
         r%=100
+        output += `${nota100} nota(s) de R$ 100\n`
     }
     if(r>=50){
         nota50=Math.trunc(r/50)
         r%=50
+        output += `${nota50} nota(s) de R$ 50\n`
     }
     if(r>=20){
         nota20=Math.trunc(r/20)
         r%=20
+        output += `${nota20} nota(s) de R$ 20\n`
     }
     if(r>=10){
         nota10=Math.trunc(r/10)
         r%=10
+        output += `${nota10} nota(s) de R$ 10\n`
     }
     if(r>=5){
         nota5=Math.trunc(r/5)
         r%=5
+        output += `${nota5} nota(s) de R$ 5\n`
     }
     if(r>=2){
         nota2=Math.trunc(r/2)
         r%=2
+        output += `${nota2} nota(s) de R$ 2\n`
     }
     if(r==1){
         nota1=1
         r%=1
+        output += `${nota1} nota(s) de R$ 1\n`
     }
-    return `${nota100} nota(s) de R$ 100.
-${nota50} nota(s) de R$ 50.
-${nota20} nota(s) de R$ 20.
-${nota10} nota(s) de R$ 10.
-${nota2} nota(s) de R$ 2.
-${nota1} nota(s) de R$ 1.`
+    return output
 }
-//let valor = prompt("Digite o valor a ser sacado:")
-console.log(cashMachine(383))
+console.log(cashMachine(70));
